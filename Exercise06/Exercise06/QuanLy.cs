d0acab4074e19c84650f2e0114aa3749b081f6e7
ThanhTanNguyen2007 <tan.187pm20569@vanlanguni.vn>
2019-10-16 18:43:16 +0700

    Cập nhật

--- Exercise06/Exercise06/QuanLy.cs
+++ Exercise06/Exercise06/QuanLy.cs
@@ -38,13 +38,18 @@ namespace Exercise06
         }
         public void WithDrawUser()
         {
-            Console.WriteLine("xin mời nhập khoảng tiền");
+            Console.WriteLine("xin mời nhập khoảng tiền :     (Ấn Q để quay trở lại menu)");
             String ADDuser = Console.ReadLine();
             int a = 0;
             if (Int32.TryParse(ADDuser, out a))
             {
                 WithDraw(a);
                 Console.WriteLine(" bạn đã thêm {0} vào trong tài khoản",a);
+            }
+            else if (ADDuser.ToUpper().Equals("Q"))
+            {
+                Console.Clear();
+                Showmenu();
             }
             else
             {
@@ -53,13 +58,19 @@ namespace Exercise06
         }
         public void DepositUser()
         {
-            Console.WriteLine("xin mời nhập khoảng tiền");
+            
+            Console.WriteLine("xin mời nhập khoảng tiền :     (Ấn Q để quay trở lại menu)");
             String ADDuser = Console.ReadLine();
             int a = 0;
             if (Int32.TryParse(ADDuser, out a))
             {
                 Deposit(a);
-                Console.WriteLine(" bạn đã rút {0} Đồng vào trong tài khoản", a);
+                Console.WriteLine(" bạn đã rút {0} Đồng vào khỏi tài khoản", a);
+            }
+            else if (ADDuser.ToUpper().Equals("Q"))
+            {
+                Console.Clear();
+                Showmenu();
             }
             else
             {
@@ -84,17 +95,23 @@ namespace Exercise06
                 switch (a)
                 {
                     case 1:
+                        Console.WriteLine("you have choose DrawUser on your Account");
                         WithDrawUser();
                         break;
                     case 2:
+                        Console.WriteLine("you have choose Deposit on your Account");
                         DepositUser();
                         break;
                     case 3:
+                        Console.WriteLine("you have choose Banlance on your Account");
                         Balance();
                         break;
                     default:
                         break;
                 }
+            }else
+            {
+                Console.WriteLine("xin mời nhập lại đúng chỉ mục trong menu");
             }
         }
     }
